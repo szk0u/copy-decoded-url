f1c5ad890f5008b5c2da5d2da7aef814a14586ab
szk0u <szk0u3@gmail.com>
2017-12-31 16:58:49 +0900

    コピーの形式を追加

--- background.js
+++ background.js
@@ -4,13 +4,32 @@ browser.contextMenus.create({
     contexts: ["all", "tab"]
 });
 
+// コンテキストメニューを2つ以上、追加すると、自動的にサブメニューになる
+// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/menus#Creating_menu_items
+
+browser.contextMenus.create({
+    id: "copy-decoded-url-markdown",
+    title: "このタブのタイトルとURLをコピー(markdown形式)",
+    contexts: ["all", "tab"]
+});
+
+browser.contextMenus.create({
+    id: "copy-decoded-url-rst",
+    title: "このタブのタイトルとURLをコピー(reStructuredText形式)",
+    contexts: ["all", "tab"]
+});
+
+browser.contextMenus.create({
+    id: "copy-decoded-url-textile",
+    title: "このタブのタイトルとURLをコピー(textile形式)",
+    contexts: ["all", "tab"]
+});
+
 browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "copy-decoded-url") {
-        const decodedUrl = decodeURI(tab.url);
+    const decodedUrl = decodeURI(tab.url);
+    const title = tab.title;
 
-        // clipboard-helper.js defines function copyToClipboard.
-        const code = "copyToClipboard(" + JSON.stringify(decodedUrl) + ");";
-
+    const executeCopy = (code) => {
         browser.tabs.executeScript({
             code: "typeof copyToClipboard === 'function';",
         }).then((results) => {
@@ -31,5 +50,22 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
             // the page, for example if the tab is a privileged page.
             console.error("Failed to copy text: " + error);
         });
+    };
+
+    if (info.menuItemId === "copy-decoded-url") {
+        const code = `copyToClipboard(${JSON.stringify(decodedUrl)});`;
+        executeCopy(code);
+    } else {
+        // markdownの場合のtextをデフォルトでセット
+        let text = `[${title}](${decodedUrl})`;
+
+        if (info.menuItemId === "copy-decoded-url-rst") {
+            text = `\`${title} <${decodedUrl}>\`_`;
+        } else if (info.menuItemId === "copy-decoded-url-textile") {
+            text = `"${title}":${decodedUrl}`;
+        }
+
+        const code = `copyToClipboard(${JSON.stringify(text)});`;
+        executeCopy(code);
     }
 });
