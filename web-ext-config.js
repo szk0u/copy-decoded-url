c021a80cdce8590e6c55c4f93a59dbec23585c00
szk0u <szk0u3@gmail.com>
2018-04-23 22:38:54 +0900

    web-extコマンドでビルド等ができるように設定

---
+++ web-ext-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  build: {
+    overwriteDest: true,
+  },
+  ignoreFiles: [
+    'package.json',
+    'yarn.lock',
+    'screenshot',
+    'README.md',
+    'web-ext-config.js',
+  ],
+};
